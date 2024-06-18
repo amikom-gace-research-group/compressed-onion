1ec098f1302e8d777ebe4120793006d08a48ec87
sultan-gemilang <sultangemilang24@gmail.com>
2024-06-18 14:13:40 +0700

    Fixed fixed the naming script

--- script/Utils/change_name.sh
+++ script/Utils/change_name.sh
@@ -1,9 +1,9 @@
-mv save/student_model/S:vgg8_T:vgg11_cifar100_kd_r:0.1_a:0.9_b:0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg11_cifar100_kd_r-0.1_a-0.9_b-0.0_2/vgg8_last.pth
-mv save/student_model/S:vgg8_T:vgg13_cifar100_kd_r:0.1_a:0.9_b:0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg13_cifar100_kd_r-0.1_a-0.9_b-0.0_2/vgg8_last.pth
-mv save/student_model/S:vgg8_T:vgg16_cifar100_kd_r:0.1_a:0.9_b:0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg16_cifar100_kd_r-0.1_a-0.9_b-0.0_2/vgg8_last.pth
-mv save/student_model/S:vgg8_T:vgg19_cifar100_kd_r:0.1_a:0.9_b:0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg19_cifar100_kd_r-0.1_a-0.9_b-0.0_2/vgg8_last.pth
+mv save/student_model/Svgg8_Tvgg11_cifar100_kd_r0.1_a0.9_b0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg11_cifar100_kd_r-0.1_a-0.9_b-0.0_2/vgg8_last.pth
+mv save/student_model/Svgg8_Tvgg13_cifar100_kd_r0.1_a0.9_b0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg13_cifar100_kd_r-0.1_a-0.9_b-0.0_2/vgg8_last.pth
+mv save/student_model/Svgg8_Tvgg16_cifar100_kd_r0.1_a0.9_b0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg16_cifar100_kd_r-0.1_a-0.9_b-0.0_2/vgg8_last.pth
+mv save/student_model/Svgg8_Tvgg19_cifar100_kd_r0.1_a0.9_b0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg19_cifar100_kd_r-0.1_a-0.9_b-0.0_2/vgg8_last.pth
 
-mv save/student_model/S:vgg8_T:vgg11_cifar100_kd_r:0.1_a:0.9_b:0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg11_cifar100_kd_r-0.1_a-0.9_b-0.0_1/vgg8_last.pth
-mv save/student_model/S:vgg8_T:vgg13_cifar100_kd_r:0.1_a:0.9_b:0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg13_cifar100_kd_r-0.1_a-0.9_b-0.0_1/vgg8_last.pth
-mv save/student_model/S:vgg8_T:vgg16_cifar100_kd_r:0.1_a:0.9_b:0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg16_cifar100_kd_r-0.1_a-0.9_b-0.0_1/vgg8_last.pth
-mv save/student_model/S:vgg8_T:vgg19_cifar100_kd_r:0.1_a:0.9_b:0.0_2/vgg8_last.pth save/student_model/S-vgg8_T-vgg19_cifar100_kd_r-0.1_a-0.9_b-0.0_1/vgg8_last.pth
+mv save/student_model/Svgg8_Tvgg11_cifar100_kd_r0.1_a0.9_b0.0_1/vgg8_last.pth save/student_model/S-vgg8_T-vgg11_cifar100_kd_r-0.1_a-0.9_b-0.0_1/vgg8_last.pth
+mv save/student_model/Svgg8_Tvgg13_cifar100_kd_r0.1_a0.9_b0.0_1/vgg8_last.pth save/student_model/S-vgg8_T-vgg13_cifar100_kd_r-0.1_a-0.9_b-0.0_1/vgg8_last.pth
+mv save/student_model/Svgg8_Tvgg16_cifar100_kd_r0.1_a0.9_b0.0_1/vgg8_last.pth save/student_model/S-vgg8_T-vgg16_cifar100_kd_r-0.1_a-0.9_b-0.0_1/vgg8_last.pth
+mv save/student_model/Svgg8_Tvgg19_cifar100_kd_r0.1_a0.9_b0.0_1/vgg8_last.pth save/student_model/S-vgg8_T-vgg19_cifar100_kd_r-0.1_a-0.9_b-0.0_1/vgg8_last.pth
