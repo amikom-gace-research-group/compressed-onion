08753d990b03c444dfc9fcc83fbcc8eff8e5df63
sultan-gemilang <sultangemilang24@gmail.com>
2024-06-18 14:23:39 +0700

    Save typo WHY!?!

--- script/Utils/change_name.sh
+++ script/Utils/change_name.sh
@@ -1,7 +1,7 @@
-mv ./save/student_model/Svgg8_Tvgg11_cifar100_kd_r0.1_a0.9_b0.0_2/ ./ave/student_model/S-vgg8_T-vgg11_cifar100_kd_r-0.1_a-0.9_b-0.0_2/
-mv ./save/student_model/Svgg8_Tvgg13_cifar100_kd_r0.1_a0.9_b0.0_2/ ./ave/student_model/S-vgg8_T-vgg13_cifar100_kd_r-0.1_a-0.9_b-0.0_2/
-mv ./save/student_model/Svgg8_Tvgg16_cifar100_kd_r0.1_a0.9_b0.0_2/ ./ave/student_model/S-vgg8_T-vgg16_cifar100_kd_r-0.1_a-0.9_b-0.0_2/
-mv ./save/student_model/Svgg8_Tvgg19_cifar100_kd_r0.1_a0.9_b0.0_2/ ./ave/student_model/S-vgg8_T-vgg19_cifar100_kd_r-0.1_a-0.9_b-0.0_2/
+mv ./save/student_model/Svgg8_Tvgg11_cifar100_kd_r0.1_a0.9_b0.0_2/ ./save/student_model/S-vgg8_T-vgg11_cifar100_kd_r-0.1_a-0.9_b-0.0_2/
+mv ./save/student_model/Svgg8_Tvgg13_cifar100_kd_r0.1_a0.9_b0.0_2/ ./save/student_model/S-vgg8_T-vgg13_cifar100_kd_r-0.1_a-0.9_b-0.0_2/
+mv ./save/student_model/Svgg8_Tvgg16_cifar100_kd_r0.1_a0.9_b0.0_2/ ./save/student_model/S-vgg8_T-vgg16_cifar100_kd_r-0.1_a-0.9_b-0.0_2/
+mv ./save/student_model/Svgg8_Tvgg19_cifar100_kd_r0.1_a0.9_b0.0_2/ ./save/student_model/S-vgg8_T-vgg19_cifar100_kd_r-0.1_a-0.9_b-0.0_2/
 
 mv ./save/student_model/Svgg8_Tvgg11_cifar100_kd_r0.1_a0.9_b0.0_1/ ./save/student_model/S-vgg8_T-vgg11_cifar100_kd_r-0.1_a-0.9_b-0.0_1/
 mv ./save/student_model/Svgg8_Tvgg13_cifar100_kd_r0.1_a0.9_b0.0_1/ ./save/student_model/S-vgg8_T-vgg13_cifar100_kd_r-0.1_a-0.9_b-0.0_1/
